bc640b417aa0604dc5887e45451e7897440b1fbc
Anindya Roy Chowdhury <anindya23@gmail.com>
2019-08-25 11:00:06 +0600

    Directory structure developed

---
+++ main.py
@@ -0,0 +1,11 @@
+import application
+import config
+import pytesseract
+from PIL import Image, ImageFilter, ImageEnhance
+
+def extract_nid():
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
