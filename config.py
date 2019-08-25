f3e523210d7e1f907ac9c2e00bc8b6c36d4e5614
Anindya Roy Chowdhury <anindya23@gmail.com>
2019-08-25 19:34:53 +0600

    Excluded config file from gitignore

---
+++ config.py
@@ -0,0 +1,11 @@
+# Data Path
+RESOURCE_DIR = "./resources/"
+
+IN_FILE_DIR = "./resources/input/"
+OUT_FILE_DIR = "./resources/output/"
+
+# File name
+TEST_FILE_NAME = "input"
+
+# Logger
+LOG_CONFIG_FILE = 'logging.ini'
